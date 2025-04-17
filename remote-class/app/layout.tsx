6463110192ea0670017727f291d3ac51ad837d52
dnyaneshwar0150 <dnyaneshwar@peopleblox.io>
2025-04-18 02:30:18 +0530

    add background gradient

--- remote-class/app/layout.tsx
+++ remote-class/app/layout.tsx
@@ -1,6 +1,6 @@
 "use client";
-import { Montserrat  } from "next/font/google";
-import {AppRouterCacheProvider} from "@mui/material-nextjs/v14-appRouter"
+import { Montserrat } from "next/font/google";
+import { AppRouterCacheProvider } from "@mui/material-nextjs/v14-appRouter";
 import "./globals.css";
 import { Styledroot } from "./Styledroot";
 import { Provider } from "react-redux";
@@ -8,10 +8,9 @@ import { store } from "./redux/store";
 import { Box } from "@mui/material";
 import { Toaster } from "react-hot-toast";
 
-
 const montserrat = Montserrat({
   subsets: ["latin"],
-  weight: ["300", "400", "500", "700", "800"], 
+  weight: ["300", "400", "500", "700", "800"],
   display: "swap",
 });
 
@@ -20,32 +19,32 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
- 
   return (
-    <html lang="en">
+    <html lang='en'>
       <body className={montserrat.className}>
-      <Provider store={store}>
-      <AppRouterCacheProvider>
-      <Box
+        <Provider store={store}>
+          <AppRouterCacheProvider>
+            <Box
               sx={{
                 position: "relative",
                 minHeight: "100vh",
-                backgroundColor: "var(--dark-grey)",
+                backgroundColor: "#1e1f26",
+                backgroundImage: `radial-gradient(rgba(255,255,255,0.08) 3px, transparent 3px)`,
+                backgroundSize: "25px 25px",
                 display: "flex",
                 justifyContent: "center",
                 alignItems: "center",
               }}
             >
-            
-             <Styledroot>{children}</Styledroot>
-
+              <Styledroot>{children}</Styledroot>
             </Box>
 
-            <Toaster   position="top-center" 
-                   reverseOrder={false}  />
-        </AppRouterCacheProvider> 
+            <Toaster
+              position='top-center'
+              reverseOrder={false}
+            />
+          </AppRouterCacheProvider>
         </Provider>
-
       </body>
     </html>
   );
